ec7a3c015a7ebec7c146af529bad27a8bf5aed6a
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-12-11 07:08:28 +0300

    add proper validation to inputitem

--- include/inputitem.h
+++ include/inputitem.h
@@ -2,11 +2,19 @@
 #define INPUTITEM_H
 
 #include <QLineEdit>
+#include <QValidator>
 
 class InputItem : public QLineEdit
 {
 public:
     InputItem(QWidget* parent = nullptr);
+    QValidator::State validate();
+
+private slots:
+    void displayBorder(const QString &text);
+
+private:
+    QValidator* validator;
 };
 
 #endif // INPUTITEM_H
--- src/inputitem.cpp
+++ src/inputitem.cpp
@@ -6,7 +6,30 @@ InputItem::InputItem(QWidget* parent):QLineEdit(parent)
 {
     setMaxLength(25);
     QRegularExpression rx(R"((0|(-?[1-9]\d{0,2}(((\s\d{3})*)|(\d*))))([.,]\d+)?)");
-    QValidator *validator = new QRegularExpressionValidator(rx, this);
-    this->setValidator(validator);
+    validator = new QRegularExpressionValidator(rx, this);
+    connect(this, &QLineEdit::textChanged, this, &InputItem::displayBorder);
     setText("0");
+}
+
+QValidator::State InputItem::validate()
+{
+    QString validationText = text();
+    int cursorPosition = 0;
+    return validator->validate(validationText, cursorPosition);
+}
+
+void InputItem::displayBorder(const QString &text)
+{
+    switch(validate())
+    {
+        case QValidator::State::Invalid:
+            this->setStyleSheet("border: 2px solid red");
+            break;
+        case QValidator::State::Intermediate:
+            this->setStyleSheet("border: 2px solid orange");
+            break;
+        case QValidator::State::Acceptable:
+            this->setStyleSheet("border: 2px solid green");
+            break;
+    }
 }
